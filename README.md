39075a44761f8929b35245b311a4637faaf4718c
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-08-16 11:13:49 +0530

    feat: Assignment 1 Console_Calculator

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# 🎯 How to use C# Starter Repo for C# assignments 
- 1. While creating new repo add the “CSharp_Starter_Repo” template repository
-![Using Template repo](https://github.com/solitontech/CSharp_Starter_Repo/blob/main/docs/assets/Images/HowToUseStarterRepo_Images/HowToUseStarterRepo.jpg) 
- 2. Make your account as the owner of the repository and include all the branches.
- 3. Add description to your repository if needed.
- 4. Select private/public repository based on the needs.
- 5. Now click create repository and your repository will be created.
- 6. Add git hooks scripts - [How to add git hooks](https://github.com/solitontech/CSharp_Starter_Repo/blob/main/docs/GitHooks.md)
- 7. Ensure that your branch names and commit messages follow this syntax: [GuideLines](https://github.com/solitontech/CSharp_Starter_Repo/blob/313030ea36043f4d2a6eb4258c68b31737cacc26/docs/Guidelines%20for%20GIT.md)
- 8. Add VSIX template -  [How to add VSIX template](https://github.com/solitontech/CSharp_Starter_Repo/blob/main/docs/VSIX%20template%20Manual.md)
- 9. Open the solution file in the root directory.
- 10. While adding new project select the CSharpStarterTemplate and select the appropriate folder location.
-     (src - for assignments and tests/unit - for unit tests)
- 11. For every assignments create a new branch from main and add your project to the solution.
- 12. When build is susccessful and the project runs perfectly give PR to main branch.
- 13. After merging to main, create new branch and add new project for next assignment.
+# ASSINGMENT-1
+    For the practical excercise  a CSharp console application needs to created which has a class 'MathUtils' containing the methods like add, subtract, Multiply, Divide that performs the arithmetic operations on the integers recived from user through the main class and displays the result.
+
+ ## MathUtils Class
+        1. It has methods like add, subtract, multiply, divide to perform the arithmetic operations.
+        2. It recieves the two integers as input parameters to perform the functions.
+        3. The methods displays the results of the operations.
+## Main Class
+        1. The main class recieves two integers from user as input.
+        2. It displays the options for the different arithmetic operations.
+        3. It recives the user's perferred aithemtic operation as option.
+        4. The method for preferred arithemetic operations are called based on the option.
+        
---
+++ src/Console_App_Calculator/Task1.cs
@@ -0,0 +1,93 @@
+﻿// <copyright file="Task1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CSharp1
+{
+    /// <summary>
+    /// Class Task1 contains main method.
+    /// </summary>
+    public class Task1
+    {
+        /// <summary>
+        /// Main method recieves two integers from user
+        /// </summary>
+        /// <param name="args">reads a line</param>
+        /// <exception cref="ArgumentNullException">Throws exception when args is null</exception>
+        public static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            int integer1, integer2, option, next;
+            string num, n;
+            do
+            {
+                getint1:
+                Console.WriteLine("Enter integer1:");
+                num = Console.ReadLine();
+                if (string.IsNullOrEmpty(num))
+                {
+                    goto getint1;
+                }
+
+                try
+                {
+                    integer1 = Convert.ToInt32(num);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Enter a valid number");
+                    goto getint1;
+                }
+
+                getint2:
+                Console.WriteLine("Enter integer2:");
+                n = Console.ReadLine();
+                if (string.IsNullOrEmpty(n))
+                {
+                    goto getint2;
+                }
+
+                try
+                {
+                    integer2 = Convert.ToInt32(n);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Enter a valid input");
+                    goto getint2;
+                }
+
+                getoption:
+                Console.WriteLine("1.Addition 2.Subtraction 3.Multiplication 4.Divison");
+                Console.WriteLine("Enter the option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        MathUtils.Add(integer1, integer2);
+                        break;
+                    case 2:
+                        MathUtils.Subtract(integer1, integer2);
+                        break;
+                    case 3:
+                        MathUtils.Multiply(integer1, integer2);
+                        break;
+                    case 4:
+                        MathUtils.Divide(integer1, integer2);
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid option");
+                        goto getoption;
+                }
+
+                Console.WriteLine("Enter 1 to continue");
+                next = Convert.ToInt32(Console.ReadLine());
+            }
+            while (next == 1);
+        }
+    }
+}
