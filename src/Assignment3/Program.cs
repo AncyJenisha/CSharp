1a5308c87424f61cfccb0ff1900edcf3f8a7bcc2
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-08-28 10:41:14 +0530

    feat: Assignment3 document

---
+++ src/Assignment3/Program.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace Assignment3
+{
+    using System.Text.Json;
+
+    /// <summary>
+    /// Program class has the main method.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method has the program entry point.
+        /// </summary>
+        /// <param name="args">Input string</param>
+        private static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2023-08-25"),
+                TemperatureCelsius = 32,
+                Summary = "Warm",
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(jsonString);
+        }
+    }
+}
---
+++ src/Assignment3/WeatherForecast.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="WeatherForecast.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace Assignment3
+{
+    using System.Text.Json;
+
+    /// <summary>
+    /// WeatherForecast has date, temperature and summary as its properties.
+    /// </summary>
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// Gets or sets and sets Date value from user.
+        /// </summary>
+        /// <value>Date as string</value>
+        public DateTimeOffset Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets and set TemperatureCelsius value from user.
+        /// </summary>
+        /// <value>Tempertaure as integer</value>
+        public int TemperatureCelsius { get; set; }
+
+        /// <summary>
+        /// Gets or sets and sets Summary from user.
+        /// </summary>
+        /// <value>Summary as string</value>
+        public string? Summary { get; set; }
+    }
+}
