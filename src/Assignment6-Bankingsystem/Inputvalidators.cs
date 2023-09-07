f28203f2371c1031074ee0434b0ac78510816e12
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-09-07 11:40:21 +0530

    feat: Methods for validation added.

---
+++ src/Assignment6-Bankingsystem/Inputvalidators.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Assignment6_Bankingsystem
+{
+    internal static class Inputvalidators
+    {
+        public static int GetInput()
+        {
+            int integer;
+            string? n = Console.ReadLine();
+            while (!int.TryParse(n, out integer))
+            {
+                Console.WriteLine("Enter valid Number");
+                n = Console.ReadLine();
+            }
+
+            return integer;
+        }
+    }
+}
--- src/Assignment6-Bankingsystem/Program.cs
+++ src/Assignment6-Bankingsystem/Program.cs
@@ -20,5 +20,4 @@ namespace Assignment6_Bankingsystem
 
         }
     }
-
 }
