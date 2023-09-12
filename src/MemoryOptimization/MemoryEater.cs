3b483d355342943ba23caefbf8dc7960be925114
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-09-12 09:40:49 +0530

    feat: Initial commit.

---
+++ src/MemoryOptimization/MemoryEater.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="MemoryEater.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MemoryOptimization
+{
+    /// <summary>
+    /// MemoryEater creates a list.
+    /// </summary>
+    public class MemoryEater
+    {
+        /// <summary>
+        /// Allocate method adds integers to list.
+        /// </summary>
+        public static void Allocate()
+        {
+            while (true)
+            {
+                List<int[]>? memAlloc = new ();
+                memAlloc.Add(new int[100000000]);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ src/MemoryOptimization/Program.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MemoryOptimization
+{
+    /// <summary>
+    /// Program class has the main method.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method creates an object to use the class.
+        /// </summary>
+        private static void Main()
+        {
+            MemoryEater.Allocate();
+        }
+    }
+}
