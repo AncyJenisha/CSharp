4dfda6f72e3ecfe5e08c418d43d339bce8be0ec4
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-09-29 10:44:14 +0530

    fix: Build test fails fixed.

--- src/Assignment6/Bankingsystem/AccountType.cs
+++ src/Assignment6/Bankingsystem/AccountType.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="EnumOptions.cs" company="PlaceholderCompany">
+﻿// <copyright file="AccountType.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
