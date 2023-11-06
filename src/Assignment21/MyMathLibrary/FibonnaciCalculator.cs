d0e7ef8d4c435a630f389b3507291fff604c879b
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-11-06 15:39:43 +0530

    feat: class library created.

---
+++ src/Assignment21/MyMathLibrary/FibonnaciCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace MyMathLibrary
+{
+    using MathNet.Numerics;
+    using System.Numerics;
+
+    /// <summary>
+    /// FibonnaciCalculator - Has method to generate fibonnaci sequence upto given number.
+    /// </summary>
+    public class FibonnaciCalculator
+    {
+        /// <summary>
+        /// CalculateFibonnaci - Gets a number and returns an array of fibonacci sequence upto the number.
+        /// </summary>
+        /// <param name="number">The length of the fibonnaci sequence to be generated.</param>
+        /// <returns>An array of fibonacci sequence.</returns>
+        public BigInteger[] CalculateFibonnaci(int number)
+        {
+            var fibonnaciSequence = Generate.Fibonacci(number);
+            return fibonnaciSequence;
+        }
+    }
+}
