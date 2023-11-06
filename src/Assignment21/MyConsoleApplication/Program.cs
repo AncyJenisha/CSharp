34e037c9648beb0e64a927dc8c1129b5d85be3ca
Ancy Jenisha Vijayaraj <Ancy.Vijayaraj@solitontech.com>
2023-11-06 20:03:14 +0530

    feat: nuget package created.

---
+++ src/Assignment21/MyConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MyConsoleApplication
+{
+    using System.Numerics;
+    using MyMathLibrary;
+
+    /// <summary>
+    /// Program class has main method which is the entry of the program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method calls the fibonaccisequence method to generate the fibonacciSequence.
+        /// </summary>
+        private static void Main()
+        {
+            int lengthOfFibonnaciSequence;
+            FibonnaciCalculator fibonnaciCalculator = new FibonnaciCalculator();
+            Console.WriteLine("Enter the length of the Fibonacci sequence:\n");
+            lengthOfFibonnaciSequence = InputValidators.GetIntegerInput();
+            BigInteger[] fibonnaciSequence = fibonnaciCalculator.CalculateFibonnaci(lengthOfFibonnaciSequence);
+
+            for (int i = 0; i < fibonnaciSequence.Length; i++)
+            {
+                Console.WriteLine(fibonnaciSequence[i]);
+            }
+        }
+    }
+}
